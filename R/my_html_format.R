bcd6c97b78c3854764989f576c7d16c95a1e87a8
lukun06 <53520104+lukun06@users.noreply.github.com>
2019-08-14 18:50:50 +0800

    Update my_html_format.R

--- R/my_html_format.R
+++ R/my_html_format.R
@@ -22,7 +22,7 @@ my_html_format = function(toc = TRUE, ...) {
     toc_float = TRUE,
     fig_width = 6.5,
     fig_height = 4,
-    theme = "readable",
+    theme = "Paper",
     code_folding = "none",
     css = css,
     number_sections = TRUE,
